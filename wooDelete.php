2b3060f650167c045773f9c8640ff5e87bf6c9c6
Mauran <mauran@linux.com>
2016-08-30 14:36:44 +0000

    Applied fixes from StyleCI

--- wooDelete.php
+++ wooDelete.php
@@ -10,50 +10,48 @@
  */
 
 // By default wp_ for standard WP-installations
-$prefix = "wp_";
+$prefix = 'wp_';
 
 // Database infos here
-$host = "localhost";
-$username = "user";
-$password = "password";
-$db = "dbname";
+$host = 'localhost';
+$username = 'user';
+$password = 'password';
+$db = 'dbname';
 
 $conn = new mysqli($host, $username, $password, $db);
 
 
 if ($conn->connect_error) {
-    die("Whoops: " . $conn->connect_error);
-} 
+    die('Whoops: '.$conn->connect_error);
+}
 
 
-$sql = "DELETE FROM " . $prefix . "woocommerce_order_itemmeta";
+$sql = 'DELETE FROM '.$prefix.'woocommerce_order_itemmeta';
 
-if ($conn->query($sql) === TRUE) {
-    echo "Deleted woocommerce_order_itemmeta " . PHP_EOL;
+if ($conn->query($sql) === true) {
+    echo 'Deleted woocommerce_order_itemmeta '.PHP_EOL;
 } else {
-    echo "Whoops: " . $conn->error;
+    echo 'Whoops: '.$conn->error;
 }
 
 
 
-$sql = "DELETE FROM " . $prefix . "woocommerce_order_items";
+$sql = 'DELETE FROM '.$prefix.'woocommerce_order_items';
 
-if ($conn->query($sql) === TRUE) {
-    echo "Deleted woocommerce_order_items" . PHP_EOL;
+if ($conn->query($sql) === true) {
+    echo 'Deleted woocommerce_order_items'.PHP_EOL;
 } else {
-    echo "Whoops: " . $conn->error;
+    echo 'Whoops: '.$conn->error;
 }
 
 
 
-$sql = "DELETE FROM " . $prefix . "posts WHERE post_type = 'shop_order'";
+$sql = 'DELETE FROM '.$prefix."posts WHERE post_type = 'shop_order'";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Deleted posts where post_type = 'shop_order'" . PHP_EOL;
+if ($conn->query($sql) === true) {
+    echo "Deleted posts where post_type = 'shop_order'".PHP_EOL;
 } else {
-    echo "Whoops: " . $conn->error;
+    echo 'Whoops: '.$conn->error;
 }
 
-echo "Fixed! Your woocommerce orders should be empty! :-)";
-
-?>
+echo 'Fixed! Your woocommerce orders should be empty! :-)';
